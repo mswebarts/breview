23ce571acb6d114243d94df7da4c6a9942b1b46c
MS Shohan <36455759+msshohan@users.noreply.github.com>
2022-10-23 21:43:17 +0600

    Fix Add Review title display

--- inc/disable-form.php
+++ inc/disable-form.php
@@ -102,7 +102,7 @@ function msbr_product_new_review_tab_content() {
                         $commenter    = wp_get_current_commenter();
                         $comment_form = array(
                             /* translators: %s is product title */
-                            'title_reply'         => have_comments() ? esc_html__( 'Add a review', 'breview' ) : sprintf( esc_html__( 'Be the first to review &ldquo;%s&rdquo;', 'breview' ), get_the_title() ),
+                            'title_reply'         => $comments ? esc_html__( 'Add a review', 'breview' ) : sprintf( esc_html__( 'Be the first to review &ldquo;%s&rdquo;', 'breview' ), get_the_title() ),
                             /* translators: %s is product title */
                             'title_reply_to'      => esc_html__( 'Leave a Reply to %s', 'breview' ),
                             'title_reply_before'  => '<span id="reply-title" class="comment-reply-title">',
