ef29d5b777a3fae1196b6ba5ef2f74797ab3d2dd
MS Shohan <36455759+msshohan@users.noreply.github.com>
2022-10-25 10:46:40 +0600

    Added parent settings overview page skeleton

---
+++ inc/admin/options-panel/pages/settings.php
@@ -0,0 +1,74 @@
+<div id="icon-options-general" class="icon32"></div>
+<h1><?php echo esc_html_e( 'Breview - Better Review for WooCommerce', 'breview' ); ?></h1>
+<p><?php echo esc_html_e( 'Breview adds a completely new workflow for reviews in WooCommerce. You can configure all the settings from here.', 'breview' ); ?></p>
+
+<div class="wrap msbr-settings-page">
+
+	<div id="icon-options-general" class="icon32"></div>
+	<h1><?php esc_attr_e( 'Heading', 'breview' ); ?></h1>
+
+	<div id="poststuff">
+
+		<div id="post-body" class="metabox-holder columns-2">
+
+			<!-- main content -->
+			<div id="post-body-content">
+
+                <div class="postbox">
+
+                    <button type="button" class="handlediv" aria-expanded="true" >
+                        <span class="screen-reader-text">Toggle panel</span>
+                        <span class="toggle-indicator" aria-hidden="true"></span>
+                    </button>
+                    <!-- Toggle -->
+
+                    <h2 class="hndle"><span><?php esc_attr_e( 'Main Content Header', 'breview' ); ?></span>
+                    </h2>
+
+                    <div class="inside">
+                        <p><?php esc_attr_e( 'WordPress started in 2003 with a single bit of code to enhance the typography of everyday writing and with fewer users than you can count on your fingers and toes. Since then it has grown to be the largest self-hosted blogging tool in the world, used on millions of sites and seen by tens of millions of people every day.',
+                                            'breview' ); ?></p>
+                    </div>
+                    <!-- .inside -->
+
+                </div>
+                <!-- .postbox -->
+
+			</div>
+			<!-- post-body-content -->
+
+			<!-- sidebar -->
+			<div id="postbox-container-1" class="postbox-container">
+
+                <div class="postbox">
+
+                    <button type="button" class="handlediv" aria-expanded="true" >
+                        <span class="screen-reader-text">Toggle panel</span>
+                        <span class="toggle-indicator" aria-hidden="true"></span>
+                    </button>
+                    <!-- Toggle -->
+
+                    <h2 class="hndle"><span><?php esc_attr_e(
+                                'Sidebar Content Header', 'breview'
+                            ); ?></span></h2>
+
+                    <div class="inside">
+                        <p><?php esc_attr_e( 'Everything you see here, from the documentation to the code itself, was created by and for the community. WordPress is an Open Source project, which means there are hundreds of people all over the world working on it. (More than most commercial platforms.) It also means you are free to use it for anything from your cat’s home page to a Fortune 500 web site without paying anyone a license fee and a number of other important freedoms.',
+                                            'breview' ); ?></p>
+                    </div>
+                    <!-- .inside -->
+
+                </div>
+                <!-- .postbox -->
+
+			</div>
+			<!-- #postbox-container-1 .postbox-container -->
+
+		</div>
+		<!-- #post-body .metabox-holder .columns-2 -->
+
+		<br class="clear">
+	</div>
+	<!-- #poststuff -->
+
+</div> <!-- .wrap -->
