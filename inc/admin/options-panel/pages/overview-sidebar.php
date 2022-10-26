9f3afbb8463920abdf630760ee87d45d4a68bd8b
MS Shohan <36455759+msshohan@users.noreply.github.com>
2022-10-26 13:04:25 +0600

    Added target blank to trustpilot link

--- inc/admin/options-panel/pages/overview-sidebar.php
+++ inc/admin/options-panel/pages/overview-sidebar.php
@@ -1,6 +1,6 @@
 <div class="msbr-box">
     <h3><?php echo esc_html_e( 'Enjoying Breview?', 'breview' ); ?></h3>
     <p>
-        <?php echo wp_sprintf( __( 'Leave us a review on %1s Trustpilot %2s and help spread the word!', 'breview' ), '<a href="#">', '</a>' ); ?>
+        <?php echo wp_sprintf( __( 'Leave us a review on %1s Trustpilot %2s and help spread the word!', 'breview' ), '<a href="#" target="_blank">', '</a>' ); ?>
     </p>
 </div>
