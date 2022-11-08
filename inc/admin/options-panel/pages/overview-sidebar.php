c3cfa3d63e76aeb7dd2119e90ad09c0165585dbc
MS Shohan <36455759+msshohan@users.noreply.github.com>
2022-11-09 00:00:19 +0600

    Added Trustpilot Review in the overview sidebar

--- inc/admin/options-panel/pages/overview-sidebar.php
+++ inc/admin/options-panel/pages/overview-sidebar.php
@@ -1,6 +1,11 @@
+<?php global $msbr_url; ?>
 <div class="msbr-box">
     <h3><?php echo esc_html_e( 'Enjoying Breview?', 'breview' ); ?></h3>
     <p>
-        <?php echo wp_sprintf( __( 'Leave us a review on %1s Trustpilot %2s and help spread the word!', 'breview' ), '<a href="#" target="_blank">', '</a>' ); ?>
+        <?php echo esc_html_e( 'Leave us a review on Trustpilot and help spread the word!', 'breview' ); ?>
+        <a href="<?php echo esc_url( 'https://www.trustpilot.com/evaluate/mswebarts.com?utm_medium=trustbox&utm_source=MSWebArtsBreviewOverview' ); ?>" class="msbr-add-trustpilot-review" target="_blank">
+            <?php echo esc_html_e( "Review Us on", "breview" ) . " "; ?>
+            <img src="<?php echo esc_url( $msbr_url . 'inc/admin/assets/images/trustpilot-logo.png' ); ?>" alt="<?php echo esc_attr_e( "Trustpilot Logo", "breview" ); ?>">
+        </a>
     </p>
 </div>
