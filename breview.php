34449c2bd419608a5f89b10e02c9bceecec49e6a
MS Shohan <36455759+msshohan@users.noreply.github.com>
2022-11-16 12:39:36 +0600

    Changed version information to 1.1.0

--- breview.php
+++ breview.php
@@ -2,13 +2,13 @@
 /**
  * Plugin Name: Breview - Better Review System for WooCommerce
  * Description: The way reviews should be handled in every WooCommerce websites just like the traditional marketplaces.
- * Version: 1.0.2
+ * Version: 1.1.0
  * Plugin URI: https://www.mswebarts.com/plugins/breview/
  * Author: MS Web Arts
  * Author URI: https://www.mswebarts.com/
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Tested up to: 6.1
+ * Tested up to: 6.1.1
  * Requires at least: 6.1
  * Requires PHP: 7.4
  * Text Domain: breview
