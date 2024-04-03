ab2b7bcd1be7b266d28781f659ab9d8efe5eaf73
MS Shohan <mswebarts@gmail.com>
2024-04-04 02:31:19 +0600

    Revert "removed composer and gitignore"
    This reverts commit a8887efd458652330d9d16ff570ac455feccc4c0.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-release',
+        'version' => 'dev-release',
+        'reference' => 'b0e92294d9dd99eb3189390913bb91f70c72fb91',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-release',
+            'version' => 'dev-release',
+            'reference' => 'b0e92294d9dd99eb3189390913bb91f70c72fb91',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'appsero/client' => array(
+            'pretty_version' => 'v2.0.2',
+            'version' => '2.0.2.0',
+            'reference' => 'b61c3ab21df4d44f805ee9476f9d880f8370a36b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../appsero/client',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
