80c05c08c744bfebf30b9245f484659b1afbe1ee
MS Shohan <mswebarts@gmail.com>
2026-07-10 15:33:05 +0600

    Updated version to 1.2.4 and tested compatibility up to WordPress 7.0.1 in breview.php

--- breview.php
+++ breview.php
@@ -2,13 +2,13 @@
 /**
  * Plugin Name: Breview - Order Reviews for WooCommerce
  * Description: Collect and display customer reviews on your WooCommerce store. Breview allows customers to leave reviews on the order received page and display them on the product page.
- * Version: 1.2.3
+ * Version: 1.2.4
  * Plugin URI: https://www.mswebarts.com/products/breview/
  * Author: MS Web Arts
  * Author URI: https://www.mswebarts.com/
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Tested up to: 6.5
+ * Tested up to: 7.0.1
  * Requires at least: 5.5
  * Requires PHP: 7.4
  * Text Domain: breview
